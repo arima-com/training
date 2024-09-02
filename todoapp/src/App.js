af705c75c320b477afd7da56201d27ec45f9e237
MITSUE\arima-mizuho <arima-mizuho@mitsue.co.jp>
2024-09-03 07:51:12 +0900

    :adhesive_bandage: JS-primer - Todoアプリ : コメント追加・render処理追加

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -10,20 +10,29 @@ export class App {
     const inputElement = document.getElementById("js-form-input");
     const containerElement = document.getElementById("js-todo-list");
     const todoItemCountElement = document.getElementById("js-todo-count");
+    // Todoアイテムをまとめるリスト
     const todoListElement = element`<ul></ul>`;
+    // 表示されているTodoアイテム数
     let todoItemCount = 0;
 
     formElement.addEventListener("submit", (event) => {
       // フォームが持つデフォルト処理；フォームの内容を指定したURLに送信する動作をキャンセルする
       event.preventDefault();
 
+      // 追加するTodoアイテムの要素（li要素）を作成
       const todoItemElement = element`<li>${inputElement.value}</li>`;
 
+      // TodoアイテムをtodoListElementに追加
       todoListElement.appendChild(todoItemElement);
 
+      // コンテナ要素の中身をTodoリストをまとめるList要素で上書きする
+      render(todoListElement, containerElement);
+
+      // Todoアイテム数を+1して、表示されているテキストを更新する
       todoItemCount++;
       todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
 
+      // 入力欄を空文字列にしてリセットする
       inputElement.value = "";
     });
   }
