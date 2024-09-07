4db325cdc6a7ed2a21bd9f163549804725c79c0f
arima-mizuho <am.el9@icloud.com>
2024-09-07 19:22:06 +0900

    :zap: udemy - Ajax通信 : 隣国をレンダリングする getCountryAndNeighborDate を実装。（コールバック地獄の問題あり）

--- ajaxapp_countryAPI/script.js
+++ ajaxapp_countryAPI/script.js
@@ -22,9 +22,40 @@ const countriesContainer = document.querySelector(".countries");
     });
   };
 
-  const renderCountry = function (data) {
+  const getCountryAndNeighborDate = function (country) {
+    // AJAX call country 1
+    const request = new XMLHttpRequest();
+    request.open("GET", `https://restcountries.com/v2/name/${country}`);
+    request.send();
+
+    request.addEventListener("load", function () {
+      const [data] = JSON.parse(this.responseText);
+
+      renderCountry(data);
+
+      // Get neighbour country 2
+      const neighbour = data.borders?.[0];
+
+      if (!neighbour) return;
+
+      // AJAX call country 2
+      const requestNeighbour = new XMLHttpRequest();
+      requestNeighbour.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`);
+      requestNeighbour.send();
+
+      requestNeighbour.addEventListener("load", function () {
+        const data = JSON.parse(this.responseText);
+
+        console.log(data);
+
+        renderCountry(data, "neighbour");
+      });
+    });
+  };
+
+  const renderCountry = function (data, className = "") {
     const html = `
-    <article class="country">
+    <article class="country ${className}">
         <img class="country__img" src="${data.flag}" />
         <div class="country__data">
             <h3 class="country__name">${data.name}</h3>
@@ -39,7 +70,9 @@ const countriesContainer = document.querySelector(".countries");
     countriesContainer.style.opacity = 1;
   };
 
-  getCountryDate("japan");
-  getCountryDate("usa");
-  getCountryDate("germany");
+  //   getCountryDate("japan");
+  //   getCountryDate("usa");
+  //   getCountryDate("germany");
+
+  getCountryAndNeighborDate("usa");
 }
