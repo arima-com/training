5b67302e9b7c9fe128c2c9c2852934512ff132af
arima-mizuho <am.el9@icloud.com>
2024-09-07 16:37:44 +0900

    :sparkles: udemy - Ajax通信 : XML HTTPリクエストでREST Countries APIより情報を取得・レンダリング

---
+++ ajaxapp_countryAPI/script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+
+///////////////////////////////////////
+
+/**
+ * XML HTTPリクエスト
+ * OLD way
+ */
+{
+  const getCountryDate = function (country) {
+    const request = new XMLHttpRequest();
+    request.open("GET", `https://restcountries.com/v2/name/${country}`);
+    request.send();
+
+    request.addEventListener("load", function () {
+      const [data] = JSON.parse(this.responseText);
+
+      const html = createCountryHTML(data);
+      countriesContainer.insertAdjacentHTML("beforeend", html);
+      countriesContainer.style.opacity = 1;
+    });
+  };
+
+  const createCountryHTML = function (data) {
+    return `
+    <article class="country">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        </div>
+    </article>
+    `;
+  };
+
+  getCountryDate("japan");
+  getCountryDate("usa");
+  getCountryDate("germany");
+}
