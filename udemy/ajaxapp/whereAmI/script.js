28af88b62ee91f6dbff5b48bd479700cfd198a3a
arima-mizuho <am.el9@icloud.com>
2024-09-14 10:32:13 +0900

    :zap: udemy - Ajax通信 : エラー処理追加

--- udemy/ajaxapp/whereAmI/script.js
+++ udemy/ajaxapp/whereAmI/script.js
@@ -69,14 +69,31 @@ const getPosition = function () {
 
 {
   const whereAmI = async function () {
-    const pos = await getPosition();
-    const { latitude: lat, longitude: lng } = pos.coords;
-    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-    const dataGeo = await resGeo.json();
-    console.log(dataGeo);
-    const res = await fetch(`https://restcountries.com/v2/name/${dataGeo.country}`);
-    const data = await res.json();
-    renderCountry(data[0]);
+    try {
+      // Geolocation
+      const pos = await getPosition();
+      const { latitude: lat, longitude: lng } = pos.coords;
+
+      // Reverse geocoding
+      const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+      if (!resGeo.ok) {
+        throw new Error(`Problem getting location data`);
+      }
+
+      const dataGeo = await resGeo.json();
+
+      // Country data
+      const res = await fetch(`https://restcountries.com/v2/name/${dataGeo.country}`);
+
+      if (!res.ok) {
+        throw new Error(`Problem getting country`);
+      }
+
+      const data = await res.json();
+      renderCountry(data[0]);
+    } catch (err) {
+      console.error(`Somethins went wrong 💥 (${err.message})`);
+    }
   };
   whereAmI();
 }
