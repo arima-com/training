fc15d3f7965f3b2026022f52123e152f221debc1
arima-mizuho <am.el9@icloud.com>
2024-09-14 09:05:51 +0900

    :truck: udemy - Ajax通信 : ブロックスコープからグローバルスコープに移動

--- udemy/ajaxapp/whereAmI/script.js
+++ udemy/ajaxapp/whereAmI/script.js
@@ -5,27 +5,37 @@ const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
 
+const renderCountry = function (data) {
+  const html = `
+      <article class="country">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+              <h3 class="country__name">${data.name}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+              <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+              <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+          </div>
+      </article>
+      `;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+};
+
+const renderError = function (message) {
+  countriesContainer.insertAdjacentText("beforeend", message);
+};
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   (position) => console.log(position),
+    //   (err) => console.error(err)
+    // );
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
 {
-  const renderCountry = function (data) {
-    const html = `
-        <article class="country">
-            <img class="country__img" src="${data.flag}" />
-            <div class="country__data">
-                <h3 class="country__name">${data.name}</h3>
-                <h4 class="country__region">${data.region}</h4>
-                <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
-                <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-                <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-            </div>
-        </article>
-        `;
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-  };
-
-  const renderError = function (message) {
-    countriesContainer.insertAdjacentText("beforeend", message);
-  };
-
   const whereAmI = function () {
     getPosition()
       .then((pos) => {
@@ -52,16 +62,6 @@ const countriesContainer = document.querySelector(".countries");
       .finally(() => {
         countriesContainer.style.opacity = 1;
       });
-  };
-
-  const getPosition = function () {
-    return new Promise(function (resolve, reject) {
-      // navigator.geolocation.getCurrentPosition(
-      //   (position) => console.log(position),
-      //   (err) => console.error(err)
-      // );
-      navigator.geolocation.getCurrentPosition(resolve, reject);
-    });
   };
 
   btn.addEventListener("click", whereAmI);
