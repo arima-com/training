ed804b79271bc700b1d4171547e35961eeeba072
arima-mizuho <am.el9@icloud.com>
2024-09-15 16:35:16 +0900

    :sparkles: udemy - Ajax通信 : async/awaitを使用したloadNPause関数・画像パズの配列から複数画像を表示するloadAllを実装

--- udemy/ajaxapp/createImage/script.js
+++ udemy/ajaxapp/createImage/script.js
@@ -27,6 +27,7 @@
   };
 
   let currentImage;
+  /*
   createImage("img/img-1.jpg")
     .then((img) => {
       currentImage = img;
@@ -49,4 +50,39 @@
     .error((err) => {
       console.error(err.message);
     });
+    */
+
+  const loadNPause = async function () {
+    try {
+      let img = await createImage("img/img-1.jpg");
+      await wait(2);
+      img.style.display = "none";
+
+      img = await createImage("img/img-2.jpg");
+      await wait(2);
+      img.style.display = "none";
+
+      img = await createImage("img/img-3.jpg");
+      await wait(2);
+      img.style.display = "none";
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  // loadNPause();
+
+  const imgArr = ["img/img-1.jpg", "img/img-2.jpg", "img/img-3.jpg"];
+
+  const loadAll = async function (imgArr) {
+    try {
+      const imgs = await Promise.all(imgArr.map(async (img) => await createImage(img)));
+      imgs.forEach((img) => img.classList.add("parallel"));
+      console.log(imgs);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  loadAll(imgArr);
 }
