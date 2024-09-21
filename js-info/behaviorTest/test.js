14af3c0bb3ce1d9ab828fec7fa598f76f4a1c606
arima-com <am.el9@icloud.com>
2024-09-21 21:52:42 +0900

    :zap: JS-info - code quality : テストの更新・実装した関数の更新

--- js-info/behaviorTest/test.js
+++ js-info/behaviorTest/test.js
@@ -1,12 +1,55 @@
 "use strict";
 
-// x の n乗をするpow(x, n)
+// x の n乗をするpow(x, n) nは自然数
 // 関数が何をすべきか（specification, spec）
 
 // どのような機能か
 describe("pow", function () {
-  it("raises to n-th power", function () {
+  it("2 raises to n-th power 3 is 8", function () {
     // テストするための関数
     assert.equal(pow(2, 3), 8);
+  });
+
+  it("3 raises to n-th power 3 is 27", function () {
+    // テストするための関数
+    assert.equal(pow(3, 3), 27);
+  });
+});
+
+describe("pow", function () {
+  function makeTest(x) {
+    let expected = x * x * x;
+    it(`${x} in the pow 3 is ${expected}`, function () {
+      assert.equal(pow(x, 3), expected);
+    });
+  }
+
+  for (let x = 1; x < 6; x++) {
+    makeTest(x);
+  }
+});
+
+describe("pow", function () {
+  // 他のテストではmakeTestは使用しないので、descriptionのネストでグループ化
+  describe("raises x to power n", function () {
+    function makeTest(x) {
+      let expected = x * x * x;
+      it(`${x} in the pow 3 is ${expected}`, function () {
+        assert.equal(pow(x, 3), expected);
+      });
+    }
+
+    for (let x = 1; x < 6; x++) {
+      makeTest(x);
+    }
+  });
+
+  // 他のテスト...
+  it("for negative n the result is NaN", function () {
+    assert.isNaN(pow(2, -1));
+  });
+
+  it("for non-integer n the result is NaN", function () {
+    assert.isNaN(pow(2, 1.5));
   });
 });
