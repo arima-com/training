f7b4a301e947d7765f24a8e23ad4675902a27e85
MITSUE\arima-mizuho <arima-mizuho@mitsue.co.jp>
2025-04-04 16:57:13 +0900

    :sparkles: ～23 済

--- udemy/typescript/arrays-objects.ts
+++ udemy/typescript/arrays-objects.ts
@@ -10,3 +10,26 @@ let users: Array<string|number>;
 users = [1, 'Max'];
 users = [30, 20];
 users = ['Smith', 'John'];
+
+let possibleResults: [number, number]; // [1, -1]
+
+possibleResults = [1, 2];
+// possibleResults = [1, 2, 3];
+
+let user: {
+    name: string;
+    age: number|string;
+    hobbies: string[];
+    role: {
+        description: string;
+        id: number;
+    }
+} = {
+    name: 'Max',
+    age: 'inifinity',
+    hobbies: ['Sports', 'Cooking'],
+    role: {
+        description: 'Developer',
+        id: 1
+    }
+};
